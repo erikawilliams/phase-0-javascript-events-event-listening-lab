730e9e13a416bfcf646868f5ae63827a2e6575b0
Erika Williams <erika@erikawilliams.us>
2022-03-21 08:27:51 -0600

    commit tree 234e4ee9b2c491d3c9bf0a568e0b9e1c6a07ed97

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
 }
+
+// const input = document.getElementById('input');
+// // const main = document.getElementById('main')
+// function clickAlert(){
+//     alert('I was clicked!')
+// }
+
+// input.addEventListener('click', clickAlert);
+// main.addEventListener('click',clickAlert)
+
